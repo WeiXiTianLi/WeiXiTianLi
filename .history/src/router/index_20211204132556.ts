42d20a7cdc02b7fcc29c2979ff1e157c4ab568b4
Venti <73828848+zhouhaixian@users.noreply.github.com>
2021-12-05 17:54:14 +0800

    Delete .history directory

--- .history/src/router/index_20211204132556.ts
+++
@@ -1,6 +0,0 @@
-import { createRouter, createWebHistory} from 'vue-router'
-
-const routes = [
-  { path: "/", component: () => import('@/views/Home.vue') },
-  { path: "/readme", component: () => import('@/views/Readme.vue')}
-]
--- .history/src/router/index_20211204132836.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory} from 'vue-router'
-
-const routes = [
-  { path: "/", component: () => import('@/views/Home.vue') },
-  { path: "/readme", component: () => import('@/views/Readme.vue')}
-]
-
-const router = VueRouter.cr
-export default router
