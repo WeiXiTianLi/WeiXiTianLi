5f62975c8c2075288dff83b61c6e7874f75660cd
Venti <73828848+zhouhaixian@users.noreply.github.com>
2021-12-05 13:18:48 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Markdown from 'vite-plugin-md'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Markdown(),
+  ],
+  base: "/WeiXiTianTianLi/dist",
+});
