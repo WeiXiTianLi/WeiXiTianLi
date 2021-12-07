2f78d8e23deb20820e1d6bfbefe303764f0b1b04
Venti <2223540637@qq.com>
2021-12-08 00:04:19 +0800

    Update THeader.vue

--- src/components/THeader.vue
+++ src/components/THeader.vue
@@ -117,60 +117,56 @@ smallMenu_show = unref(smallMenu_show)
         <el-menu-item index="2-1">Gitee</el-menu-item>
         <el-menu-item index="2-2">Github</el-menu-item>
       </el-sub-menu>
-      <el-menu-item>
-        <el-link href="https://github.com/WeiXiTianLi/TianLi" :underline="false" target="_blank">
-          Github
-          <svg
-            class="icon outbound"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            x="0px"
-            y="0px"
-            viewBox="0 0 100 100"
-            width="15"
-            height="15"
-            data-v-b8818f8c
-          >
-            <path
-              fill="currentColor"
-              d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-            />
-            <polygon
-              fill="currentColor"
-              points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-            />
-          </svg>
-        </el-link>
-      </el-menu-item>
-      <el-menu-item>
-        <el-link
-          href="https://gitee.com/Yu_Sui_Xian/GenshinImpact_AutoMap"
-          :underline="false"
-          target="_blank"
+      <el-link href="https://github.com/WeiXiTianLi/TianLi" :underline="false" target="_blank">
+        Github
+        <svg
+          class="icon outbound"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          x="0px"
+          y="0px"
+          viewBox="0 0 100 100"
+          width="15"
+          height="15"
+          data-v-b8818f8c
         >
-          Gitee
-          <svg
-            class="icon outbound"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            x="0px"
-            y="0px"
-            viewBox="0 0 100 100"
-            width="15"
-            height="15"
-            data-v-b8818f8c
-          >
-            <path
-              fill="currentColor"
-              d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-            />
-            <polygon
-              fill="currentColor"
-              points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-            />
-          </svg>
-        </el-link>
-      </el-menu-item>
+          <path
+            fill="currentColor"
+            d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
+          />
+          <polygon
+            fill="currentColor"
+            points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
+          />
+        </svg>
+      </el-link>
+      <el-link
+        href="https://gitee.com/Yu_Sui_Xian/GenshinImpact_AutoMap"
+        :underline="false"
+        target="_blank"
+      >
+        Gitee
+        <svg
+          class="icon outbound"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          x="0px"
+          y="0px"
+          viewBox="0 0 100 100"
+          width="15"
+          height="15"
+          data-v-b8818f8c
+        >
+          <path
+            fill="currentColor"
+            d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
+          />
+          <polygon
+            fill="currentColor"
+            points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
+          />
+        </svg>
+      </el-link>
     </el-sub-menu>
   </el-menu>
 </template>
@@ -181,11 +177,12 @@ smallMenu_show = unref(smallMenu_show)
   transition-duration: 0.2s;
   color: var(--el-text-color-primary);
 }
-.el-menu > .el-menu-item > .el-link {
-  padding: 0 0px;
-}
-.el-menu > .el-menu-iem > .el-link > .el-link--inner {
-  height: 1rem;
+.el-menu > .el-link {
+  display: flex;
+  margin: 0;
+  padding: 10px;
+  padding-left: 10px;
+  justify-content: flex-start;
 }
 svg {
   vertical-align: middle;
