f368e92e934ae09e2234676d2ab6f4a938d8a93c
Venti <2223540637@qq.com>
2021-12-08 23:06:17 +0800

    Update THeader.vue
    修复了菜单点击事件无操作问题

--- src/components/THeader.vue
+++ src/components/THeader.vue
@@ -5,7 +5,7 @@ import { Expand } from '@element-plus/icons'
 
 const url = `https://${window.location.host}`;
 const handleSelect = (key: any, keyPath: any) => {
-  switch (keyPath[0]) {
+  switch (key) {
     case 'doc-TianLi_Map':
       router.push({ path: '/tianli_map/doc' });
       break;
@@ -87,34 +87,6 @@ menu_Collapse = unref(menu_Collapse)
         />
       </svg>
     </el-link>
-    <!--     <el-link
-      v-if="!menu_Collapse"
-      href="https://gitee.com/Yu_Sui_Xian/GenshinImpact_AutoMap"
-      :underline="false"
-      target="_blank"
-    >
-      Gitee
-      <svg
-        class="icon outbound"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-        x="0px"
-        y="0px"
-        viewBox="0 0 100 100"
-        width="15"
-        height="15"
-        data-v-b8818f8c
-      >
-        <path
-          fill="currentColor"
-          d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-        />
-        <polygon
-          fill="currentColor"
-          points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-        />
-      </svg>
-    </el-link>-->
     <el-sub-menu v-else style="margin-left: auto;" @select="handleSelect">
       <template #title>
         <el-icon>
@@ -156,33 +128,6 @@ menu_Collapse = unref(menu_Collapse)
           />
         </svg>
       </el-link>
-      <!--       <el-link
-        href="https://gitee.com/Yu_Sui_Xian/GenshinImpact_AutoMap"
-        :underline="false"
-        target="_blank"
-      >
-        Gitee
-        <svg
-          class="icon outbound"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          x="0px"
-          y="0px"
-          viewBox="0 0 100 100"
-          width="15"
-          height="15"
-          data-v-b8818f8c
-        >
-          <path
-            fill="currentColor"
-            d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-          />
-          <polygon
-            fill="currentColor"
-            points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-          />
-        </svg>
-      </el-link>-->
     </el-sub-menu>
   </el-menu>
 </template>
