25f56ec3c96a79b137b5908b15b7aa2adf70cce7
zhouhaixian <2223540637@qq.com>
2021-12-10 21:27:49 +0800

    Localization

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -25,6 +25,26 @@ export default defineUserConfig<DefaultThemeOptions>({
         selectLanguageText: "选择语言",
         selectLanguageAriaLabel: "选择语言",
         selectLanguageName: "简体中文",
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdatedText: "上次更新",
+        contributorsText: "贡献者",
+        tip: "提示",
+        warning: "注意",
+        danger: "警告",
+        notFound: [
+          "这里什么都没有",
+          "我们怎么到这来了？",
+          "这是一个 404 页面",
+          "看起来我们进入了错误的链接",
+          "锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷",
+          "烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫",
+        ],
+        backToHome: "返回首页",
+
+        //a11y
+        openInNewWindow: "在新窗口打开",
+        toggleDarkMode: "切换夜间模式",
+        toggleSidebar: "切换侧边栏",
       },
       "/en/": {
         navbar: navbar.en,
